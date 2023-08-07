897da71e0ac594d06a03e8054f9cd5f55b348ae0
Szymon Kępiński <szymon.kepinski@profil-software.com>
2023-08-07 08:15:29 +0200

    STC-103: request.user always exists

--- djangostock/application/auth.py
+++ djangostock/application/auth.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import BasePermission
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.is_admin
+        return request.user.is_authenticated and request.user.is_admin
 
 
 class UnauthenticatedPost(BasePermission):
