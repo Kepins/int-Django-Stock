2e561d389d7074bd6065997fbe88cf3ae4132a7f
Szymon Kępiński <szymon.kepinski@profil-software.com>
2023-08-21 12:38:53 +0200

    STC-113: Add nginx

--- djangostock/settings/base.py
+++ djangostock/settings/base.py
@@ -32,7 +32,7 @@ API_KEY_TWELVEDATA = config("API_KEY_TWELVEDATA")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost", cast=Csv())
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1, localhost", cast=Csv())
 
 AUTH_USER_MODEL = "application.User"
 
